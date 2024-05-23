165263ea4623031aea0fb06cbd68370ce5aa6f9a
dnlbtlnt <169160983+dnlbtlnt@users.noreply.github.com>
2024-05-23 13:59:56 +0800

    Add files via upload

---
+++ MyNFTs.js
@@ -0,0 +1,26 @@
+const NFTs = [];
+
+function mintNFT(_name, _eyeColor, _shirtType, _bling) {
+    const NFT = {
+        "name": _name,
+        "eyeColor": _eyeColor,
+        "shirtType": _shirtType,
+        "bling": _bling
+    };
+    NFTs.push(NFT);
+    console.log("Minted: " + _name);
+}
+
+function listNFTs() {
+    for (let i = 0; i < NFTs.length; i++) {
+        console.log(NFTs[i]);
+    }
+}
+
+function getTotalSupply() {
+    console.log(NFTs.length);
+}
+
+mintNFT("Alice", "Brown", "T-Shirt", "Diamond Necklace");
+listNFTs();
+getTotalSupply();
